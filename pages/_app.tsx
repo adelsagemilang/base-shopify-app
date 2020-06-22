2fe71852a123f7d243e578689a3631f51ca4769d
Adelsa Gemilang Fadila <adelsagemilangfadila@gmail.com>
2020-06-22 15:55:42 +0700

    Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import App from 'next/app';
+import Head from 'next/head';
+import { AppProvider } from '@shopify/polaris';
+import { Provider } from '@shopify/app-bridge-react';
+import Cookies from "js-cookie";
+import '@shopify/polaris/styles.css';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import translations from '@shopify/polaris/locales/en.json';
+
+const client = new ApolloClient({
+  fetchOptions: {
+    credentials: 'include'
+  },
+});
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+    const apiKey =  JSON.stringify(process.env.SHOPIFY_API_KEY);
+    const config = { apiKey, shopOrigin: Cookies.get("shopOrigin"), forceRedirect: true };
+
+    return (
+      <React.Fragment>
+        <Head>
+          <title>Sample App</title>
+          <meta charSet="utf-8" />
+        </Head>
+        <Provider config={config}>
+          <AppProvider i18n={translations}>
+            <ApolloProvider client={client}>
+              <Component {...pageProps} />
+            </ApolloProvider>
+          </AppProvider>
+        </Provider>
+      </React.Fragment>
+    );
+  }
+}
+
+export default MyApp;
